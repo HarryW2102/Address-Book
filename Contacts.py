b00956d1ffffbaf75737e3780d900862f387676c
c1007593 <h.wedge2@newcastle.ac.uk>
2021-10-26 12:30:21 +0100

    Called class attributes and allowed user to input name,address and phone number and also prints it with a custom message

--- Contacts.py
+++ Contacts.py
@@ -3,8 +3,19 @@ import sys
 class contact:
     def __init__(self, name, phone_num, address):
         self.name = name
-        self.phonenum = phone_num
+        self.phone_num = phone_num
         self.address = address
+
+if __name__=="__main__":
+    con1 = contact(input("Enter your first name "), input("Enter your phone number "), input("Enter your address "))
+    print("Age: " + con1.name)
+    print("Phone Number: " + con1.phone_num)
+    print("Address: " + con1.address)
+
+
+
+
+
 
 
 
