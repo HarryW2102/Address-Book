f4bf88e18c3b35b646baceb57c017ed326c44cef
c1007593 <h.wedge2@newcastle.ac.uk>
2021-10-26 12:40:50 +0100

    Added dob

--- Contacts.py
+++ Contacts.py
@@ -1,16 +1,19 @@
 import sys
 
 class contact:
-    def __init__(self, name, phone_num, address):
+    def __init__(self, name, phone_num, address, dob):
         self.name = name
         self.phone_num = phone_num
         self.address = address
+        self.dob = dob
 
 if __name__=="__main__":
-    con1 = contact(input("Enter your first name "), input("Enter your phone number "), input("Enter your address "))
+    con1 = contact(input("Enter your first name "), input("Enter your phone number "), input("Enter your address "),input("Enter your dob "))
     print("Age: " + con1.name)
     print("Phone Number: " + con1.phone_num)
     print("Address: " + con1.address)
+    print("Date of Birth: " + con1.dob)
+
 
 
 
