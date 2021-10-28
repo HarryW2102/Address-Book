23e40dc4883c1d6b10da354ae5f03e62e1e668a6
c1007593 <h.wedge2@newcastle.ac.uk>
2021-10-28 14:34:23 +0100

    Added a while loop so more than one contact can be added

--- Contacts.py
+++ Contacts.py
@@ -7,22 +7,23 @@ class contact:
         self.address = address
         self.dob = dob
 
-
-
-
-
 if __name__=="__main__":
-    answer = input
-    answer = input("Do you want to add another contact Yes/No")
-    if answer == 'Yes':
+
+    answer = input("Do you want to add another contact Yes/No ")
+    while answer == 'Yes':
         con1 = contact(input("Enter your first name "), input("Enter your phone number "), input("Enter your address "),
                        input("Enter your dob "))
         print("Age: " + con1.name)
         print("Phone Number: " + con1.phone_num)
         print("Address: " + con1.address)
         print("Date of Birth: " + con1.dob)
-    elif answer == 'No':
+        answer = input("Do you want to add another contact Yes/No ")
+    else:
         print("No more contacts will be added")
+
+
+
+
 
 
 
