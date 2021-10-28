377dc2fbd5b209e95c9f3bb5a3c556b47cadeaf3
c1007593 <h.wedge2@newcastle.ac.uk>
2021-10-28 14:12:10 +0100

    Added a yes or no function to whether the user wants to add a new contact or not using a if statement

--- Contacts.py
+++ Contacts.py
@@ -7,12 +7,32 @@ class contact:
         self.address = address
         self.dob = dob
 
+
+
+
+
 if __name__=="__main__":
-    con1 = contact(input("Enter your first name "), input("Enter your phone number "), input("Enter your address "),input("Enter your dob "))
-    print("Age: " + con1.name)
-    print("Phone Number: " + con1.phone_num)
-    print("Address: " + con1.address)
-    print("Date of Birth: " + con1.dob)
+    answer = input
+    answer = input("Do you want to add another contact Yes/No")
+    if answer == 'Yes':
+        con1 = contact(input("Enter your first name "), input("Enter your phone number "), input("Enter your address "),
+                       input("Enter your dob "))
+        print("Age: " + con1.name)
+        print("Phone Number: " + con1.phone_num)
+        print("Address: " + con1.address)
+        print("Date of Birth: " + con1.dob)
+    elif answer == 'No':
+        print("No more contacts will be added")
+
+
+
+
+
+
+
+
+
+
 
 
 
