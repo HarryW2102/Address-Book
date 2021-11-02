d2c62b9b241afcac06b77663b488665a30d8adc1
c1007593 <h.wedge2@newcastle.ac.uk>
2021-11-02 10:22:47 +0000

    Allowed program to write user info into a file from where it can later be taken from

--- Contacts.py
+++ Contacts.py
@@ -11,8 +11,8 @@ if __name__=="__main__":
 
     answer = input("Do you want to add another contact Yes/No ")
     while answer == 'Yes':
-        con1 = contact(input("Enter your first name "), input("Enter your phone number "), input("Enter your address "),
-                       input("Enter your dob "))
+        con1 = contact(input(str("Enter your first name ")), input(str("Enter your phone number ")), input(str("Enter your address ")),
+                       input(str("Enter your dob ")))
         print("Age: " + con1.name)
         print("Phone Number: " + con1.phone_num)
         print("Address: " + con1.address)
@@ -21,12 +21,12 @@ if __name__=="__main__":
     else:
         print("No more contacts will be added")
 
+        Info = "Age: " + con1.name, "Phone Number: " + con1.phone_num, "Address: " + con1.address, "Date of Birth: " + con1.dob
 
+        file = open('contactinfo.txt','a')
+        file.write('\n' +str(Info))
+        file.close()
 
-    file = open('contactinfo.txt','a')
-    file.write('\n' + str(contact))
-    file.close()
-    
 
 
 
