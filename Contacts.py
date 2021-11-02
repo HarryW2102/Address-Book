36e74a6732ab13077d65e86ccc7820fa5819a564
c1007593 <h.wedge2@newcastle.ac.uk>
2021-11-02 10:26:37 +0000

    Multiple user information can be saved in one go

--- Contacts.py
+++ Contacts.py
@@ -9,6 +9,7 @@ class contact:
 
 if __name__=="__main__":
 
+
     answer = input("Do you want to add another contact Yes/No ")
     while answer == 'Yes':
         con1 = contact(input(str("Enter your first name ")), input(str("Enter your phone number ")), input(str("Enter your address ")),
@@ -18,14 +19,15 @@ if __name__=="__main__":
         print("Address: " + con1.address)
         print("Date of Birth: " + con1.dob)
         answer = input("Do you want to add another contact Yes/No ")
+        Info = "Age: " + con1.name, "Phone Number: " + con1.phone_num, "Address: " + con1.address, "Date of Birth: " + con1.dob
+        file = open('contactinfo.txt', 'a')
+        file.write('\n' + str(Info))
+        file.close()
     else:
         print("No more contacts will be added")
 
-        Info = "Age: " + con1.name, "Phone Number: " + con1.phone_num, "Address: " + con1.address, "Date of Birth: " + con1.dob
 
-        file = open('contactinfo.txt','a')
-        file.write('\n' +str(Info))
-        file.close()
+
 
 
 
